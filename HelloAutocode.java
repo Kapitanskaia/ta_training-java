9811c4815effac613bd0f18416d6d8f32a96234a
Anna <131599567+Kapitanskaia@users.noreply.github.com>
2023-08-02 14:43:55 +0400

    Delete HelloAutocode.java

--- HelloAutocode.java
+++
@@ -1,8 +0,0 @@
-package com.epam.training.student_anna_kapitanskaia.classes;
-
-public class HelloAutocode {
-    public static void main(String[] args) {
-        //Write a program, printing "Hello, Autocode!"
-        System.out.println("Hello, Autocode!");
-    }
-}
